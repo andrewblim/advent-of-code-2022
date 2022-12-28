1af8116cf98e23661cb9d309d12f50f2e5082002
Andrew Lim <andrew.b.lim@gmail.com>
2022-12-27 19:34:50 -0500

    Finalize day19 problem1

--- elixir/lib/day19.ex
+++ elixir/lib/day19.ex
@@ -33,20 +33,32 @@ defmodule Day19 do
     }
   end
 
+  def augment_blueprint(blueprint) do
+    max_costs = for resource_type <- [:ore, :clay, :obs, :geode], into: %{} do
+      max_cost = [:ore, :clay, :obs, :geode]
+      |> Enum.map(fn robot_type -> blueprint[robot_type][resource_type] end)
+      |> Enum.max()
+      {resource_type, max_cost}
+    end
+    Map.put(blueprint, :max_costs, max_costs)
+  end
+
   def score_blueprints(blueprints, n) do
+    init_state = %{
+      resources: %{ore: 0, clay: 0, obs: 0, geode: 0},
+      robots: %{ore: 1, clay: 0, obs: 0, geode: 0},
+      turns_left: n,
+    }
     for blueprint <- blueprints, into: %{} do
-      init_state = %{
-        resources: %{ore: 0, clay: 0, obs: 0, geode: 0},
-        robots: %{ore: 1, clay: 0, obs: 0, geode: 0},
-        turns_left: n,
-      }
+      blueprint = augment_blueprint(blueprint)
       visited = bfs(blueprint, MapSet.new([init_state]), MapSet.new())
       final_scores = Enum.map(visited, &score_state/1)
-      {blueprint[:id], {MapSet.size(visited), Enum.max(final_scores)}}
+      {blueprint[:id], Enum.max(final_scores)}
     end
   end
 
   def bfs(blueprint, states, visited) do
+    # IO.inspect(MapSet.size(visited))
     if MapSet.size(states) == 0 do
       visited
     else
@@ -54,6 +66,7 @@ defmodule Day19 do
       next_states =
         for state <- states,
             next_robot_type <- [:ore, :clay, :obs, :geode],
+            not is_nil(state[:resources][next_robot_type]),
             build_state = build(blueprint, state, next_robot_type),
             not is_nil(build_state),
             not MapSet.member?(visited, build_state),
@@ -64,12 +77,8 @@ defmodule Day19 do
     end
   end
 
-  def run_to_end(state) do
-    produce(state, state[:turns_left])
-  end
-
   def score_state(state) do
-    run_to_end(state)[:resources][:geode]
+    produce(state, state[:turns_left])[:resources][:geode]
   end
 
   def build(blueprint, state, robot_type) do
@@ -77,14 +86,37 @@ defmodule Day19 do
     if not is_nil(t) and t < state[:turns_left] do
       # produce for t turns
       # then pay for the robot and build it
-      state
+      state = state
       |> produce(t)
-      |> update_in([:resources, :ore], fn x -> x - blueprint[robot_type][:ore] end)
-      |> update_in([:resources, :clay], fn x -> x - blueprint[robot_type][:clay] end)
-      |> update_in([:resources, :obs], fn x -> x - blueprint[robot_type][:obs] end)
+      |> update_in([:resources, :ore], fn x ->
+        if not is_nil(x), do: x - blueprint[robot_type][:ore]
+      end)
+      |> update_in([:resources, :clay], fn x ->
+        if not is_nil(x), do: x - blueprint[robot_type][:clay]
+      end)
+      |> update_in([:resources, :obs], fn x ->
+        if not is_nil(x), do: x - blueprint[robot_type][:obs]
+      end)
       |> produce()
       |> update_in([:robots, robot_type], fn x -> x + 1 end)
+
+      # figure out when we don't need to track robots/resources any more
+      if must_track?(blueprint, state, robot_type) do
+        state
+      else
+        state
+        |> put_in([:robots, robot_type], nil)
+        |> put_in([:resources, robot_type], nil)
+      end
     end
+  end
+
+  def must_track?(blueprint, state, resource_type) do
+    # we must track a resource if we could spend more than we have now or if the rate of production
+    # is less than we could spend on a given turn
+    resource_type == :geode
+    or blueprint[:max_costs][resource_type] > state[:resources][resource_type]
+    or blueprint[:max_costs][resource_type] > state[:robots][resource_type]
   end
 
   def time_to_build(blueprint, state, robot_type) do
@@ -111,19 +143,28 @@ defmodule Day19 do
 
   def produce(state, n \\ 1) do
     state
-    |> update_in([:resources, :ore], fn x -> x + state[:robots][:ore] * n end)
-    |> update_in([:resources, :clay], fn x -> x + state[:robots][:clay] * n end)
-    |> update_in([:resources, :obs], fn x -> x + state[:robots][:obs] * n end)
+    |> update_in([:resources, :ore], fn x ->
+      if not is_nil(x), do: x + state[:robots][:ore] * n
+    end)
+    |> update_in([:resources, :clay], fn x ->
+      if not is_nil(x), do: x + state[:robots][:clay] * n
+    end)
+    |> update_in([:resources, :obs], fn x ->
+      if not is_nil(x), do: x + state[:robots][:obs] * n
+    end)
     |> update_in([:resources, :geode], fn x -> x + state[:robots][:geode] * n end)
     |> update_in([:turns_left], fn prev_n -> prev_n - n end)
   end
 
   def problem1(input \\ "data/day19.txt", type \\ :file) do
-    blueprints = read_input(input, type)
-    score_blueprints(blueprints, 24)
+    read_input(input, type)
+    |> score_blueprints(24)
+    |> Enum.reduce(0, fn {id, score}, acc -> acc + id * score end)
   end
 
   def problem2(input \\ "data/day19.txt", type \\ :file) do
     read_input(input, type)
+    |> Enum.take(3)
+    |> score_blueprints(24)
   end
 end
